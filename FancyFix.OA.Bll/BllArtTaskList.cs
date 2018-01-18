90ecdc0f1cc4fa3ba40fe41dbcbe2ee2e8efca11
余作想 <963526232@qq.com>
2018-01-18 19:18:38 +0800

    2222

---
+++ FancyFix.OA.Bll/BllArtTaskList.cs
@@ -0,0 +1,39 @@
+﻿using Dos.DataAccess.Base;
+using Dos.ORM;
+using FancyFix.OA.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyFix.OA.Bll
+{
+    public class BllArtTaskList : ServiceBase<ArtTaskList>
+    {
+        public static BllArtTaskList Instance()
+        {
+            return new BllArtTaskList();
+        }
+
+        public static IList<ArtTaskList> GetList()
+        {
+            var where = new Where<ArtTaskList>();
+            where.And(o => true);
+
+            var p = Db.Context.From<ArtTaskList>()
+                .Where(where);
+            return p.OrderBy(o => o.Id).ToList();
+        }
+
+        public static bool Add(ArtTaskList model)
+        {
+            return Insert(model) > 0;
+        }
+
+        public static bool Update(ArtTaskList model)
+        {
+            return Repository<ArtTaskList>.Update(model) > 0;
+        }
+    }
+}
---
+++ FancyFix.OA/Areas/DesignerTask/DesignerTaskAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FancyFix.OA.Areas.DesignerTask
+{
+    public class DesignerTaskAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DesignerTask";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DesignerTask_default",
+                "DesignerTask/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}

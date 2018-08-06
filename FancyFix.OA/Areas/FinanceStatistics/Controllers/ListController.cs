d3f081f13aadaf22f5332672029d051532a3b183
余作想 <963526232@qq.com>
2018-08-06 18:10:36 +0800

    update

---
+++ FancyFix.OA/Areas/FinanceStatistics/Controllers/ListController.cs
@@ -0,0 +1,27 @@
+﻿using FancyFix.OA.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FancyFix.OA.Areas.FinanceStatistics.Controllers
+{
+    public class ListController : BaseAdminController
+    {
+        /// <summary>
+        /// 财务统计
+        /// </summary>
+        /// <returns></returns>
+        // GET: FinanceStatistics/List
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        //public JsonResult PageList(int page = 0, int pagesize = 0, int displayid = 0)
+        //{
+
+        //}
+    }
+}
